8f81130ef9366c5bb57d7c66cfbaf4a27bd366ff
kevinjylin <kevinjylin@gmail.com>
2026-03-24 17:02:01 -0700

    added text and removed icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main />;
+  return <div className = "flex flex-col items-center justify-center min-h-screen py-2">
+    <div className = "text-6xl font-bold">
+      Welcome to Kevin's Personal Website!
+    </div>
+  </div>;
+
 }
